6a66bfd730921e7add6190efa73a7c7073eef9b0
barrada <barrada@me.com>
2019-12-29 14:06:09 +0200

    added almarai font

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },    
     ]
   },
   /*
@@ -30,6 +30,8 @@ export default {
   ** Global CSS
   */
   css: [
+    '~/assets/fonts/almarai.css',
+    '~/assets/css/style.css'
   ],
   /*
   ** Plugins to load before mounting the App
