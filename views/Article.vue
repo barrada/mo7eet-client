6506a27cff34fb4bb66cb1e09561899f081143ad
barrada <barrada@me.com>
2019-12-30 20:02:56 +0200

    added more strusctured data to article.vue - added var in quotes@

--- views/Article.vue
+++ views/Article.vue
@@ -70,12 +70,12 @@ export default {
         "@context": "http://schema.org",
         "@type": "Article",
         "author": "محيط",
-        "datePublished": this.date,
-        "headline": this.title,
-        "image": this.thumbnail,
+        "datePublished": `"${this.date}"`,
+        "headline": `"${this.title}"`,
+        "image": `"${this.thumbnail}"`,
         "publisher": 'محيط',
-        "dateModified": this.date,
-        "mainEntityOfPage": this.url,
+        "dateModified": `"${this.date}"`,
+        "mainEntityOfPage":`"${ this.url}"`,
         // More structured data...
       },
     };
