cef5a90aa06a90a19ed3e11c0541fa39b90d607a
barrada <barrada@me.com>
2019-12-30 19:35:32 +0200

    added more strusctured data to article.vue - still single recipe and other categories

--- views/Article.vue
+++ views/Article.vue
@@ -60,7 +60,7 @@ export default {
     
     console.log(gallery)
    
-    return { post: data[0], gallery: gallery,title:data[0].title,descritpion:data[0].body.substring(0,100) + '...',thumbnail:uploads+data[0].thumbnail,url:url };
+    return { post: data[0], gallery: gallery,title:data[0].title,descritpion:data[0].body.substring(0,100) + '...',thumbnail:uploads+data[0].thumbnail,url:url,date:data[0].post_date };
   },
   data() {
     return {
@@ -69,6 +69,13 @@ export default {
       structuredData: {
         "@context": "http://schema.org",
         "@type": "Article",
+        "@author": "محيط",
+        "@datePublished": this.date,
+        "@headline": this.title,
+        "@image": this.thumbnail,
+        "@publisher": 'محيط',
+        "@dateModified": this.date,
+        "@mainEntityOfPage": this.url,
         // More structured data...
       },
     };
