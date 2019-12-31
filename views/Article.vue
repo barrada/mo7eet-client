5e737c625604a4c65f35661f5cc8a8847e2befa2
barrada <barrada@me.com>
2019-12-31 12:06:50 +0200

    added more strusctured data to article.vue,added keywords

--- views/Article.vue
+++ views/Article.vue
@@ -60,7 +60,7 @@ export default {
     
     console.log(gallery)
    
-    return { post: data[0], gallery: gallery,title:data[0].title,bodySEO:data[0].body,descritpion:data[0].body.substring(0,100) + '...',thumbnail:uploads+data[0].thumbnail,url:url,date:data[0].post_date };
+    return { post: data[0], gallery: gallery,title:data[0].title,bodySEO:data[0].body,descritpion:data[0].body.substring(0,100) + '...',thumbnail:uploads+data[0].thumbnail,url:url,date:data[0].post_date,keywords:data[0].keywords };
   },
   data() {
     return {     
@@ -114,6 +114,7 @@ export default {
             "datePublished": "${this.date}",
             "headline": "${this.title}",
             "image": "${this.thumbnail}",
+            "keywords": "${this.keywords}",
             "publisher": {
               "@type": "Organization",
               "name": "محيط",
