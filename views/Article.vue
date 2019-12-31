a4f66e6373d0cbdce902c42b8e7859d995410673
barrada <barrada@me.com>
2019-12-31 13:25:34 +0200

    added cols  article.vue

--- views/Article.vue
+++ views/Article.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="col-12 md-5 lg-5 mx-auto">
+  <div cols="12" md="5" lg="5" class="mx-auto">
     <!-- <span>{{ new Date() | moment("dddd, MMMM Do YYYY") }}</span> -->
     <!-- <span>{{ "2019-12-24T10:47:21.000Z" | moment("from", "now") }}</span> -->
     <h1>{{post.title}}</h1> 
