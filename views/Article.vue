95b8cff16aec79d0df251b3f18d1d7ac40455764
barrada <barrada@me.com>
2020-01-05 09:32:51 +0200

    added #article to article body, removed scoped css

--- views/Article.vue
+++ views/Article.vue
@@ -19,7 +19,7 @@
 						 </adsbygoogle>
         
       </v-col>
-      <v-col id="article" cols="12" md="6" lg="6" class="mx-auto">
+      <v-col cols="12" md="6" lg="6" class="mx-auto">
    
     <!-- <span>{{ new Date() | moment("dddd, MMMM Do YYYY") }}</span> -->
     <!-- <span>{{ "2019-12-24T10:47:21.000Z" | moment("from", "now") }}</span> -->
@@ -60,7 +60,7 @@
     </v-carousel> -->
 
 
-    <div class="mt-5" v-html="post.body">
+    <div  id="article" class="mt-5" v-html="post.body">
           {{post.body}}
     </div>
         </v-col>
